d5b6ecdb93357651344aef51dd0533cc8c387645
muhammetErgul <mami177552@outlook.com>
2024-09-09 15:11:36 +0300

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,30 +5,30 @@ import { MdLocationOn, MdPhone, MdEmail } from "react-icons/md";
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="bg-[#0A568C] text-white py-5 px-5 w-full   ">
-      <div className=" mx-auto flex-col lg:flex-row justify-between items-start">
-        <div className="w-full lg:w-1/3">
-          <div className="space-y-2 ">
-            <div className="flex items-start space-x-2">
-              <MdPhone className="text-2xl mt-1" />
-              <div>
-                <p>444 0 205</p>
-              </div>
+    <footer className="bg-[#0A568C] text-white py-1 px-5 w-full">
+      <div className="mx-auto flex flex-col lg:flex-row justify-between items-start">
+        <div className="w-full lg:w-1/3 space-y-2">
+          <div className="flex items-center justify-between space-x-4">
+            <div className="flex items-center gap-1">
+              <MdPhone className="text-xl" />
+              <p>444 0 205</p>
             </div>
-            <div className="flex items-start space-x-2">
-              <MdEmail className="text-2xl mt-1" />
-              <div>
-                <a href="mailto:info@medicinehospital.com.tr">
-                  info@medicinehospital.com.tr
-                </a>
-              </div>
+            <div className="flex items-center gap-2">
+              <MdEmail className="text-xl" />
+              <a
+                href="mailto:info@medicinehospital.com.tr"
+                className="text-blue-500 hover:underline text-white"
+              >
+                info@medicinehospital.com.tr
+              </a>
             </div>
-            <div className="flex items-start space-x-2">
-              <MdLocationOn className="text-4xl mt-1" />
-              <div>
-                Barbaros Mah, H. Ahmet Yesevi Cad, No: 149 Güneşli - Bağcılar /
-                İstanbul
-              </div>
+          </div>
+
+          <div className="flex items-start space-x-2">
+            <MdLocationOn className="text-3xl" />
+            <div>
+              Barbaros Mah, H. Ahmet Yesevi Cad, No: 149 Güneşli - Bağcılar /
+              İstanbul
             </div>
           </div>
         </div>
