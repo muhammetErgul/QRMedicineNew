0be9aadad7e76423bc62620982259c72a5d5eaac
muhammetErgul <mami177552@outlook.com>
2024-09-09 14:38:02 +0300

    .

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -7,7 +7,7 @@ import AutoSignOut from "../components/AutoSignOut";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-[url('/arkaplan-min.png')] bg-no-repeat bg-cover bg-center">
+    <div className="flex flex-col min-h-screen bg-[url('/arkaplan7.png')] bg-no-repeat bg-cover bg-center">
        {/* <div className="absolute inset-0 bg-black opacity-10 z-0"></div> */}
       <Header />
       <AutoSignOut />
