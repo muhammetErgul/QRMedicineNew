ec087f2b42ced7b0766724dbea5db280f01756ae
muhammetErgul <mami177552@outlook.com>
2024-09-11 17:25:57 +0300

    .

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       {/* Arka plan resmi ekranı tam olarak kaplayacak */}
       <div 
         className="absolute inset-0 bg-[url('/Mobil.jpg')] bg-cover bg-center z-0 "
-        style={{ backgroundAttachment: 'fixed', width: '100%', height: '100vh' }} 
+        style={{  width: '100%', height: '100vh' }} 
       ></div>
 
       {/* İçerik ve şeffaf arkaplan overlay */}
