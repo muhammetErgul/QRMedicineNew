f7022ffb255f1849bc1684866865549585a723c4
muhammetErgul <mami177552@outlook.com>
2024-09-14 23:10:36 +0300

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,15 +3,17 @@ import { MdLocationOn, MdPhone, MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0A568C] text-white py-1  w-full fixed bottom-0" style={{ zIndex: 20 }}>
+    <footer
+      className="bg-[#0A568C] text-white py-1  w-full fixed bottom-0"
+      style={{ zIndex: 20 }}
+    >
       <div className="max-w-screen-xl mx-auto flex flex-col items-center    lg:flex-row lg:justify-between lg:items-center space-y-2 lg:space-y-0">
-        
         {/* Telefon ve Email Bilgileri */}
         <div className="flex  items-center gap-6 text-xs lg:text-sm">
           <div className="flex items-center gap-1">
             <MdPhone className="text-lg" />
             <a href="tel:4440205" className="hover:underline">
-              4440205
+             <small>444 0205</small>
             </a>
           </div>
           <div className="flex items-center gap-1">
@@ -20,18 +22,25 @@ const Footer = () => {
               href="mailto:info@medicinehospital.com.tr"
               className="hover:underline text-white"
             >
-              info@medicinehospital.com.tr
+              <small>info@medicinehospital.com.tr</small>
             </a>
           </div>
         </div>
 
         {/* Konum Bilgisi */}
         <div className="flex items-center text-xs lg:text-sm leading-tight gap-1">
-          <MdLocationOn className="text-lg" />
-          <div>
+          <a
+            href="https://www.google.com/maps/dir//Atlas+%C3%9Cniversitesi+Medicine+Hastanesi,+Barbaros,+Hoca+Ahmet+Yesevi+Cd.+No:+149,+34203+Ba%C4%9Fc%C4%B1lar%2F%C4%B0stanbul/@41.0306647,28.8383798,19.5z/data=!4m8!4m7!1m0!1m5!1m1!1s0x14caa4de4cd2a8e1:0xd8327c4ae8c67491!2m2!1d28.8386075!2d41.0305247?entry=ttu&g_ep=EgoyMDI0MDkwOS4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <MdLocationOn className="text-lg cursor-pointer" />
+          </a>
+          <small>
             Barbaros Mah, H. Ahmet Yesevi Cad, No: 149 Güneşli - Bağcılar /
             İstanbul
-          </div>
+          </small>
         </div>
       </div>
     </footer>
