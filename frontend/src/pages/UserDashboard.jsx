8647f72674f09bc9915ccbf1b8111adcad970c24
muhammetErgul <mami177552@outlook.com>
2024-10-03 10:57:56 +0300

    .

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,23 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import anketPng from '../assets/anket.png';
-import aboutPng from '../assets/hastane.png';
-import bilgiPng from '../assets/bilgi.png';
-import randevuPng from '../assets/randevu.png';
-import siparisPng from '../assets/siparis.png';
-import sonucPng from '../assets/sonuc.png';
-import helpDesk from '../assets/customer-support.png';
+import anketPng from "../assets/anket.png";
+import aboutPng from "../assets/hastane.png";
+
+import randevuPng from "../assets/randevu.png";
+import siparisPng from "../assets/siparis.png";
+import sonucPng from "../assets/sonuc.png";
+import helpDesk from "../assets/customer-support.png";
 
 const UserDashboard = () => {
   const { t } = useTranslation();
-
+  const [searchParams] = useSearchParams();
+  const type = searchParams.get("type");
   return (
     <div className="relative flex flex-col mt-3">
       {/* Main Content */}
       <div className="relative flex-grow container mx-auto px-12">
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center font-libre">
           {/* Kart boyutları küçültüldü */}
-          <Link to="/anketler" className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300">
+          <Link
+            to={type ? `/anketler?type=${type}` : "/anketler"}
+            className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300"
+          >
             <img
               src={anketPng}
               className="w-10 h-10 object-contain ml-1"
@@ -28,7 +32,10 @@ const UserDashboard = () => {
             </span>
           </Link>
 
-          <Link to="/tanitim" className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300">
+          <Link
+            to="/tanitim"
+            className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300"
+          >
             <img
               src={aboutPng}
               alt="Tanıtım"
@@ -67,12 +74,13 @@ const UserDashboard = () => {
               alt="Sonuç"
               className="w-10 h-10 object-contain"
             />
-            <span className="text-xs font-bold text-black">
-              {t("sonuc")}
-            </span>
+            <span className="text-xs font-bold text-black">{t("sonuc")}</span>
           </a>
 
-          <Link to="/helpdesk" className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300">
+          <Link
+            to="/helpdesk"
+            className="flex flex-col items-center justify-center w-24 h-20 gap-1 p-2 bg-white bg-opacity-50 rounded-lg shadow-md transition duration-300"
+          >
             <img
               src={helpDesk}
               alt="Bilgi"
@@ -80,7 +88,7 @@ const UserDashboard = () => {
             />
             <small className="text-xs font-bold text-black text-center ">
               {/* {t("info")} */}
-             {t("helpDesk")}
+              {t("helpDesk")}
             </small>
           </Link>
 
