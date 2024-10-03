015c79efb79b303b62f1548c865f377266ed159e
muhammetErgul <mami177552@outlook.com>
2024-10-03 10:21:10 +0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AddEditSurvey from './pages/AddEditSurvey';
 import UserDashboard from './pages/UserDashboard';
 import UserSurveyList from './components/UserSurveyList';
@@ -7,10 +7,14 @@ import Tanitim from './components/Tanitim';
 import Layout from './pages/Layout';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
-import OrdersPage from './pages/OrdersPage'; // OrdersPage bileşenini içe aktarın
+import OrdersPage from './pages/OrdersPage';
 import LoginPage from './pages/Login';
 import HelpDesk from './components/HelpDesk';
+
 function App() {
+  const params = new URLSearchParams(window.location.search);
+  const surveyType = params.get('type') || "yatan";
+
   return (
     <Router>
       <Layout>
@@ -19,12 +23,20 @@ function App() {
           <Route path="/edit-survey/:id" element={<AddEditSurvey />} />
           <Route path="/" element={<UserDashboard />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/anketler" element={<UserSurveyList />} />
+          {/* /anketler rotasını type parametresiyle yönlendir */}
+          <Route 
+            path="/anketler" 
+            element={<Navigate to={`/anketler?type=${surveyType}`} replace />} 
+          />
+          <Route 
+            path="/anketler" 
+            element={<UserSurveyList />} 
+          />
           <Route path="/tanitim" element={<Tanitim />} />
           <Route path='/helpdesk' element={<HelpDesk />} />
           <Route path="/order" element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/orders" element={<OrdersPage />} /> {/* OrdersPage için rota ekleyin */}
+          <Route path="/orders" element={<OrdersPage />} />
         </Routes>
       </Layout>
     </Router>
