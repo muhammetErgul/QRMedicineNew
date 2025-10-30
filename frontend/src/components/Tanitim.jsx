1e818f9b1dfb8d20e596ff3425b03bcec079e359
muhammetErgul <mami177552@outlook.com>
2025-10-30 22:50:24 +0300

    .

--- frontend/src/components/Tanitim.jsx
+++ frontend/src/components/Tanitim.jsx
@@ -10,8 +10,8 @@ const Tanitim = () => {
           <div className="relative w-full h-0 pb-[56.25%] md:rounded-lg overflow-hidden shadow-md">
             <iframe
               className="absolute inset-0 w-full h-full rounded-t-md md:rounded-lg"
-              src="https://www.youtube.com/watch?v=WniUSOq3tZg"
-              title="Medicine Hospital Tanitim Filmi"
+              src="https://youtu.be/WniUSOq3tZg"
+              title="Atlas Üniversitesi Hastanesi Tanitim Filmi"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
